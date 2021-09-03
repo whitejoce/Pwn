bac2d233c1874a6a9b0b4c61da34f29b56bb6b33
whitejoce <36195824+whitejoce@users.noreply.github.com>
2021-09-03 21:48:36 +0800

    Add files via upload

---
+++ About_Machine/BigOrLittleEndian.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i = 0x11223344;
+    char *p;
+
+    p = (char *) &i;
+    if (*p == 0x44) { 
+        printf("Little endian\n");
+    }
+    else {
+        printf("Big endian\n");
+    }
+    return 0;
+}
